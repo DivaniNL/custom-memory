c7eac2818118509b7444952cb862d56acde79132
DivaniNL <dylanvannierop@gmail.com>
2021-06-07 15:47:50 +0200

    Three pages Working

---
+++ CustomImagesController.swift
@@ -0,0 +1,14 @@
+//
+//  TestViewController.swift
+//  Custom-Memory
+//
+//  Created by Dylan  Nierop on 07/06/2021.
+//  Copyright © 2021 Christopher Ching. All rights reserved.
+//
+
+import UIKit
+
+class CustomImagesController: UIViewController, UICollectionViewDelegate{
+    
+    @IBOutlet var CustomImages: UIView!
+}
